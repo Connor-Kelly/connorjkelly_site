65ef126f1c36aa193dde4732477ef99dfa01804d
Connor <70863090+Connor-Kelly@users.noreply.github.com>
2023-10-28 11:53:38 -0500

    changed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,22 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
-import vercel from '@astrojs/vercel/serverless'
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), sitemap(), tailwind()],
-	output: 'server',
-	adapter: vercel({
-		webAnalytics: {
-			enabled: true,
-		},
-	}),
-})
+  site: 'https://connorjkelly.com',
+  integrations: [mdx(), sitemap(), tailwind()]  ,
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
+});
 
 // import { defineConfig } from 'astro/config';
 
