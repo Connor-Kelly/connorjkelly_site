283f994bb1d46e4a1cc563ab81317befd4b0088d
Connor <70863090+Connor-Kelly@users.noreply.github.com>
2023-10-28 11:58:00 -0500

    changed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
   integrations: [mdx(), sitemap(), tailwind()]  ,
   output: "server",
   adapter: vercel({
+    analytics: true,
     webAnalytics: {
       enabled: true,
     },
