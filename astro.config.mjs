d4014fbae118929fe3109353bb30436a45d48c78
Connor-Kelly <connorkelly726@gmail.com>
2025-02-25 13:06:33 -0600

    rescaffold into starlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,28 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-
-
-import vercel from "@astrojs/vercel/serverless";
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://connorjkelly.com',
-  integrations: [mdx(), sitemap(), tailwind()]  ,
-  output: "server",
-  adapter: vercel({
-    analytics: true,
-    webAnalytics: {
-      enabled: true,
-    },
-  })
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
 });
-
-// import { defineConfig } from 'astro/config';
-
-// export default defineConfig({
-
-// });
